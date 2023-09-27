68979a30d44d75646dc0a99e8e8ecc1b0324fe74
Julia Ruiter <julia.ruiter@str.us>
2023-09-27 10:21:02 -0700

    trained custom policies, but realised random was set to 'true' the whoe time

--- multigrid/agents_pool/__init__.py
+++ multigrid/agents_pool/__init__.py
@@ -1,7 +1,14 @@
 from multigrid.agents_pool.YourName_policies.YourPolicyName_policy_v2 import YourPolicyNameV2_Policy
 from multigrid.agents_pool.YourName_policies.YourPolicyName_policy import YourPolicyName_Policy
+from multigrid.agents_pool.YourName_policies.Custom_1_offensive import *
+from multigrid.agents_pool.YourName_policies.Custom_2_goal import Custom_2_goal_Policy
+
+
 
 SubmissionPolicies = {
     "your_policy_name": YourPolicyName_Policy,
     "your_policy_name_v2": YourPolicyNameV2_Policy,
+    "Custom_1_BAD": Custom_1_offensive_Policy,
+    "Custom_1_offensive": Custom_1_full_offensive_Policy,
+    "Custom_2_goal": Custom_2_goal_Policy
 }
